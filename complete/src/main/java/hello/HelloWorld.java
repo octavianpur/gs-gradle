2b097d77cb0a5d0462821a8441ee31ef5915a6b1
octavianpur <octavian.p@gmail.com>
2021-12-31 10:18:57 +0200

    Update HelloWorld.java

--- complete/src/main/java/hello/HelloWorld.java
+++ complete/src/main/java/hello/HelloWorld.java
@@ -2,12 +2,22 @@ package hello;
 
 import org.joda.time.LocalTime;
 
+import org.apache.log4j.Logger;
+
+import java.io.*;
+import java.sql.SQLException;
+import java.util.*;
+
+// log.debug("Hello this is a debug message");
+
 public class HelloWorld {
   public static void main(String[] args) {
     LocalTime currentTime = new LocalTime();
-    System.out.println("The current local time is: " + currentTime);
+    //System.out.println("The current local time is: " + currentTime);
+    log.debug("The current local time is: " + currentTime);
 
     Greeter greeter = new Greeter();
-    System.out.println(greeter.sayHello());
+    //System.out.println(greeter.sayHello());
+    log.debug(greeter.sayHello());
   }
 }
